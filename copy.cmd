81e35514302b5ee5b7d472948ecb70d672dd3b9e
0cch <0cch.bk@gmail.com>
2017-08-16 19:19:45 +0800

    fix icon

--- copy.cmd
+++ copy.cmd
@@ -1,1 +1,3 @@
-xcopy .\_site\* ..\0CCh.github.io\ /s /v /y
+xcopy .\public\* ..\0CCh.github.io\ /s /v /y
+xcopy .\uploads ..\0CCh.github.io\ /s /v /y
+xcopy .\favicon.ico ..\0CCh.github.io\ /s /v /y
